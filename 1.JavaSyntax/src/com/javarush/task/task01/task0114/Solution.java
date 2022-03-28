64cfcb0b9a3d6cd2fc07c05466a67ddb5ecfcf5a
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-03-28 12:25:33 +0300

    add task0114

--- 1.JavaSyntax/src/com/javarush/task/task01/task0114/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0114/Solution.java
@@ -7,6 +7,17 @@ JavaRush. Learn once - use anywhere
 public class Solution {
     public static void main(String[] args) {
         //Напишите тут ваш код
-
+        String text = "JavaRush. Learn once - use anywhere"; //помещаю текст в переменную
+        //вывожу на экран значение переменной 10 раз
+        System.out.println(text); //1
+        System.out.println(text); //2
+        System.out.println(text); //3
+        System.out.println(text); //4
+        System.out.println(text); //5
+        System.out.println(text); //6
+        System.out.println(text); //7
+        System.out.println(text); //8
+        System.out.println(text); //9
+        System.out.println(text); //10
     }
 }
