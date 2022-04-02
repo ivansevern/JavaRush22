9898287658ad19264060c88527519affe1e7fd46
ivansevern <ivansevern@gmail.com>
2022-04-02 16:33:55 +0300

    reload

--- 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
@@ -7,5 +7,9 @@ package com.javarush.task.task01.task0142;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        int a = 5; //присваиваю переменным тип и значение
+        int b = 7;
+        System.out.println(a + b); //вывожу сумму
+        System.out.println(a * b); //вывожу произведение
     }
 }
