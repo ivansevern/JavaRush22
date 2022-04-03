20d9bceb1516b70964ce97059326322a8e7d147b
ivansevern <ivansevern@gmail.com>
2022-04-03 11:23:10 +0300

    add task0218

--- 1.JavaSyntax/src/com/javarush/task/task02/task0218/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0218/Solution.java
@@ -1,13 +1,21 @@
 package com.javarush.task.task02.task0218;
 
-/**
- * Method print3 display 3 times
- */
+/*
+Повторенье-мать
+*/
+
 public class Solution {
     public static void print3(String text){
         //write this code
-        for (int i = 1; i <= 3 ; i++)
+        //так
        System.out.println(text);
+       System.out.println(text);
+       System.out.println(text);
+
+        /* или так
+        for (int i = 1; i <= 3 ; i++)
+            System.out.println(text);
+            */
 
     }
 
