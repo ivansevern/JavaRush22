c53bacaab53c72b17fad4a78b39b869bcfd158f8
ivansevern <ivansevern@gmail.com>
2022-04-03 15:01:26 +0300

    add task0303

--- 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
@@ -1,15 +1,20 @@
 package com.javarush.task.task03.task0303;
 
+/*
+Обмен валют
+*/
+
 public class Solution {
     public static void main(String[] args) {
         //write this code
-        System.out.println(convertEurToUsd(1, 10));
-        System.out.println(convertEurToUsd(10, 10));
+        double usd1 = convertEurToUsd(100, 0.8);
+        double usd2 = convertEurToUsd(200, 0.8);
+        System.out.println(usd1);
+        System.out.println(usd2);
     }
 
     public static double convertEurToUsd(int eur, double exchangeRate){
         //write this code
-       double out = eur * exchangeRate;
-               return out;
+        return eur * exchangeRate;
     }
 }
