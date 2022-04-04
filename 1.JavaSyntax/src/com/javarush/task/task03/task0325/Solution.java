39e1de94f6b50893bd4097ee122dc3e07c347b8b
ivansevern <ivansevern@gmail.com>
2022-04-04 21:44:14 +0300

    add task0325

--- 1.JavaSyntax/src/com/javarush/task/task03/task0325/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0325/Solution.java
@@ -3,11 +3,16 @@ package com.javarush.task.task03.task0325;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/*
+Финансовые ожидания
+*/
+
 public class Solution {
     public static void main(String[] args) throws Exception{
-        //writethis code
+        //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(reader.readLine());
-        System.out.println("Я буду зарабатывать $"+n+" в час");
+        String salary = reader.readLine();
+        String text = "Я буду зарабатывать $" + salary +" в час";
+        System.out.println(text);
     }
 }
