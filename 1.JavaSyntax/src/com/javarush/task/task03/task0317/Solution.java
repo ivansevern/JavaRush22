5e17dcf362190adc3ed76ca98a163f4c62bf6eb6
ivansevern <ivansevern@gmail.com>
2022-04-04 19:38:54 +0300

    add task0317

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0317/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task03.task0317;
+
+/*
+Путь самурая
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        char a = 0x65E5;
+        char b = 0x672C;
+        char c = 0x8A9E;
+        System.out.println("" + a + b + c);
+    }
+}
