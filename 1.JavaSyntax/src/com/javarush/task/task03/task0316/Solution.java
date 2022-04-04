e7bac12e7b4b7fc8ecbaf88ad2ffd5df32a1c579
ivansevern <ivansevern@gmail.com>
2022-04-04 19:22:42 +0300

    add task0316

--- 1.JavaSyntax/src/com/javarush/task/task03/task0316/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0316/Solution.java
@@ -1,20 +1,13 @@
 package com.javarush.task.task03.task0316;
 
-/**
- * вывести на экан текст - 2 строки
- * It's Windows path: "C:\Program Files\Java\jdk1.7\bin"
- * It's Java string: \"C:\\Program Files\Java\jdk1.7\bin"
- */
 /*
-\" - ээкранирование двойной кавычки
-\\ - экранирование обратной косой черты (\)
- */
+Экранирование символов
+*/
 
 public class Solution {
     public static void main(String[] args) {
+        //напишите тут ваш код
         System.out.println("It's Windows path: \"C:\\Program Files\\Java\\jdk1.7\\bin\"");
         System.out.println("It's Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7\\\\bin\\\"");
     }
-    //writethis code
-
 }
