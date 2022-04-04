962dc5b534172e80974be8cbf4a239d57c775f7b
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-04 12:29:55 +0300

    add task0312

--- 1.JavaSyntax/src/com/javarush/task/task03/task0312/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0312/Solution.java
@@ -1,22 +1,18 @@
 package com.javarush.task.task03.task0312;
 
-/**
- * Add method public static int convertToSeconds(int hour)
- * which should covert hours to sec
- * Вызови его 2 раза в методе main с люьыми параметрами
- * результат вывести на экран каждый раз сновой строки
- */
+/*
+Конвертируем время
+*/
+
 public class Solution {
     //write this code
     public static int convertToSeconds(int hour) {
-
-        int out = hour * 3600;
-        return out;
+        return hour * 60 * 60;
     }
 
         public static void main (String[]args){
             //write this code
-            System.out.println(convertToSeconds(1));
             System.out.println(convertToSeconds(10));
+            System.out.println(convertToSeconds(12));
         }
     }
