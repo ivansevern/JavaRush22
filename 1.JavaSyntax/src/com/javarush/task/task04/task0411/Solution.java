af89faf19de880d9f7b2ef7937cbc1acf4042789
ivansevern <ivansevern@gmail.com>
2022-04-05 21:35:20 +0300

    add task0411

--- 1.JavaSyntax/src/com/javarush/task/task04/task0411/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0411/Solution.java
@@ -1,10 +1,9 @@
 package com.javarush.task.task04.task0411;
 
-/**
- * по номеру месяца
- * метод должен определить время года (зима, лето, осень, весна)
- *
- */
+/*
+Время года
+*/
+
 public class Solution {
     public static void main(String[] args) {
         checkSeason(12);
@@ -14,13 +13,39 @@ public class Solution {
     }
 
     public static void checkSeason(int month) {
-        //wwrite this code
-        if (month >= 3 && month <= 5)    // если номер месяца >= 3 или <= 5 то выводим сообщение
+        //write this code
+        switch (month) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("осень");
+                break;
+            default:
+                System.out.println("неверный номер месяца");
+                break;
+        }
+
+   /*     if (month >= 3 && month <= 5)    // если номер месяца >= 3 или <= 5 то выводим сообщение
             System.out.println("весна");
         else if (month >= 6 && month <= 8) //иначе если
             System.out.println("лето");
         else if (month >= 9 && month <= 11)
             System.out.println("осень");
-        else System.out.println("зима"); //иначе остальные варианты
+        else System.out.println("зима"); //иначе остальные варианты*/
     }
 }
