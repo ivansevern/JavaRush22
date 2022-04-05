35ba2c781c555da0b45a12f38d2b1ebf0f38a318
ivansevern <ivansevern@gmail.com>
2022-04-05 21:51:55 +0300

    add task0412

--- 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
@@ -3,26 +3,20 @@ package com.javarush.task.task04.task0412;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-/**
- * Ввести с клав число
- * если число положительное, то увеличить его в 2 раза
- * если число отрицательное, то прибавить 1
- * если число равное 0, то вывести 0
- */
+/*
+Положительное и отрицательное число
+*/
+
 public class Solution {
     public static void main(String[] args) throws Exception {
         //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
-        if (a > 0)
-        //    a = a*2;
-        System.out.println(a*2);
-        else if (a < 0)
-          //  a = a++;
-        System.out.println(++a);
-        else
-          //  a = 0;
-            System.out.println(a);
-
+        if (a > 0) {         //если положительное
+            a *= 2;          //то умножаем число на 2
+        } else if (a < 0) {  //если отрицательное
+            a += 1;          //то прибавляем к числу 1
+        }
+        System.out.println(a);
     }
 }
