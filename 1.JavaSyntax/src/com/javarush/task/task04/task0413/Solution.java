40c86881b1096bfd41e64b803e076a18e5f5fc76
ivansevern <ivansevern@gmail.com>
2022-04-05 22:07:50 +0300

    add task0413

--- 1.JavaSyntax/src/com/javarush/task/task04/task0413/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0413/Solution.java
@@ -4,31 +4,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Ввести с клав номер дня недели
- * в зависимости от номера вывести название
- * понедельник
- * вторник
- * среда
- * четверг
- * пятница
- * суббота
- * воскресенье
- * если больше 7 или меньше 1 вывести
- * такого дня не существует
+/*
+День недели
  */
 public class Solution {
     public static void main(String[] args) throws IOException {
         //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(reader.readLine());
-        if (n == 1) System.out.println("понедельник");
-        else if (n == 2) System.out.println("вторник");
-        else if (n == 3) System.out.println("среда");
-        else if (n == 4) System.out.println("четверг");
-        else if (n == 5) System.out.println("пятница");
-        else if (n == 6) System.out.println("суббота");
-        else if (n == 7) System.out.println("воскресенье");
-        else System.out.println("такого дня не существует");
+        int day = Integer.parseInt(reader.readLine());
+        switch (day) {
+            case 1:
+                System.out.println("понедельник");
+                break;
+            case 2:
+                System.out.println("вторник");
+                break;
+            case 3:
+                System.out.println("среда");
+                break;
+            case 4:
+                System.out.println("четверг");
+                break;
+            case 5:
+                System.out.println("пятница");
+                break;
+            case 6:
+                System.out.println("суббота");
+                break;
+            case 7:
+                System.out.println("воскресенье");
+                break;
+            default:
+                System.out.println("такого дня не существует");
+                break;
+        }
+
+        /*if (day == 1) System.out.println("понедельник");
+        else if (day == 2) System.out.println("вторник");
+        else if (day == 3) System.out.println("среда");
+        else if (day == 4) System.out.println("четверг");
+        else if (day == 5) System.out.println("пятница");
+        else if (day == 6) System.out.println("суббота");
+        else if (day == 7) System.out.println("воскресенье");
+        else System.out.println("такого дня не существует");*/
     }
 }
