1e5d1d2bcfaf9624480d3eb463fcd7ce42cefd73
ivansevern <ivansevern@gmail.com>
2022-04-07 20:01:45 +0300

    add task0428

--- 1.JavaSyntax/src/com/javarush/task/task04/task0428/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0428/Solution.java
@@ -1,24 +1,30 @@
 package com.javarush.task.task04.task0428;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Ввести с клав 3 целых числа
- * Вывести кол-во положительных чисел из них
- */
+/*
+Положительное число
+*/
+
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
         int c = Integer.parseInt(reader.readLine());
-        int count=0;                //инициализурую счетчик
-        if (a>0) { count++; }       //выполняю условие
-        if (b>0) {count++;}
-        if (c>0) {count++;}
+
+        int count = 0;  //инициализурую счетчик
+        if (a > 0) {
+            count++;
+        }
+        if (b > 0) {
+            count++;
+        }
+        if (c > 0) {
+            count++;
+        }
         System.out.println(count);
     }
 }
