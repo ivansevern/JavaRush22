2a2751c9d272a858c05259d7dca7b89a6c0a0ba7
ivansevern <ivansevern@gmail.com>
2022-04-07 19:04:46 +0300

    add task0425

--- 1.JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
@@ -1,35 +1,27 @@
 package com.javarush.task.task04.task0425;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Ввести с клав 2 целых числа, которые будут координатами точки, не лежахей на осях координат OX и OY
- * вывести номер координатной четверти, в которой находится точка
- *
- * для 1 четверти - a>0 и b>0
- * для 2 четверти - a<0 и b>0
- * для 3 четверти - a<0 и b<0
- * для 4 четверти - a>0 и b<0
- *
- * для
- * 46 - 1
- * -6-6 - 3
- */
+/*
+Цель установлена
+*/
+
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
-        if (a>0 & b>0) {
-            System.out.println(1);}
-        else if (a<0 & b>0) {
-            System.out.println(2);}
-        else if (a<0 & b<0) {
-            System.out.println(3);}
-        else if (a>0 & b<0) {
-            System.out.println(4);}
+
+        if (a > 0 && b > 0) {
+            System.out.println(1);
+        } else if (a < 0 && b > 0) {
+            System.out.println(2);
+        } else if (a < 0 && b < 0) {
+            System.out.println(3);
+        } else if (a > 0 && b < 0) {
+            System.out.println(4);
+        }
     }
 }
