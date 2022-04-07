d38c6b20123c666492436b91d1293a9b8c623883
ivansevern <ivansevern@gmail.com>
2022-04-07 19:28:36 +0300

    add task0426

--- 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
@@ -1,28 +1,40 @@
 package com.javarush.task.task04.task0426;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * ВВести с клав целое число
- * вывести надпись строку описание следующего вида
- * Отрицательное число
- * Отрицательное нечетное число
- * ноль
- * Положительное четное число
- * Положительное нечетное число
- *
- */
+/*
+Ярлыки и числа
+*/
+
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
-        if (a<0 && a%2==0) System.out.println("Отрицательное число");
-        else if (a<0 && a%2!=0) System.out.println("Отрицательное нечетное число");
-        else if (a==0) System.out.println("ноль");
-        else if (a>=0 && a%2==0) System.out.println("Положительное четное число");
-        else if (a>=0 && a%2!=0) System.out.println("Положительное нечетное число");
+
+        if (a == 0) {
+            System.out.println("ноль");
+        } else if (a > 0 && a % 2 == 0) {
+            System.out.println("Положительное четное число");
+        } else if (a > 0 && a % 2 != 0) {
+            System.out.println("Положительное нечетное число");
+        } else if (a < 0 && a % 2 == 0) {
+            System.out.println("Отрицательное четное число");
+        } else if (a < 0 && a % 2 != 0) {
+            System.out.println("Отрицательное нечетное число");
+        }
+
+        /*if (a < 0 && a % 2 == 0) {
+            System.out.println("Отрицательное число");
+        } else if (a < 0 && a % 2 != 0) {
+            System.out.println("Отрицательное нечетное число");
+        } else if (a == 0) {
+            System.out.println("ноль");
+        } else if (a > 0 && a % 2 == 0) {
+            System.out.println("Положительное четное число");
+        } else if (a > 0 && a % 2 != 0) {
+            System.out.println("Положительное нечетное число");
+        }*/
     }
 }
