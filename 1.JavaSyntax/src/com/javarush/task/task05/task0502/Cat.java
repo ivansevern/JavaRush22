fda9fbaf741c6b126a014ed4fb2c58a19a45fa8f
ivansevern <ivansevern@gmail.com>
2022-04-08 21:01:37 +0300

    add task0502

--- 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
@@ -1,42 +1,34 @@
 package com.javarush.task.task05.task0502;
 
-/**
- * Реализуй метод boolean fight(Cat anotherCat)
- * Подумай механизм драки котов в зависимости от их веса, возвраста и силы
- * Сравни каждый критерий по отдельности, чтобы победил у кого больше критериев лучше
- * Метод должен определять, выиграли ли мы (this) бой или нет true - при победе, False - если проиграли
- * если ничья false+++
- */
+/*
+Реализовать метод fight
+*/
+
 public class Cat {
     public int age;
     public int weight;
-    public int strenght;
+    public int strength;
 
     public Cat() {
     }
 
     public boolean fight(Cat anotherCat) {
         //write this code
+      int ageScore = Integer.compare(this.age, anotherCat.age);
+      int weightScore = Integer.compare(this.weight, anotherCat.weight);
+      int strengthScore = Integer.compare(this.strength, anotherCat.strength);
+
+      int count = ageScore + weightScore + strengthScore;
+      return  count >0;
+    }
+
+    public static void main(String[] args) {
+    }
+}
 /*compare - сравнивает(x, y)
 если (x==y) - возвращает 0
      (x<y) - возвращает -1
      (x>y) - возвращает 1
  */
-
-      int ageCount = Integer.compare(this.age, anotherCat.age);
-      int weightCount = Integer.compare(this.weight, anotherCat.weight);
-      int strenghtCount = Integer.compare(this.strenght, anotherCat.strenght);
-
-      int count = ageCount + weightCount + strenghtCount;
-      return  count >0;
-
-    }
-
-
-    public static void main(String[] args) {
-    }
-
-
-}
 
 
