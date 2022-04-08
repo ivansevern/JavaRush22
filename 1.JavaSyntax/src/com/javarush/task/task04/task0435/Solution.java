c61fe40caf929e59c8563ed78697abadfab831e2
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-08 12:44:35 +0300

    add task0435

--- 1.JavaSyntax/src/com/javarush/task/task04/task0435/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0435/Solution.java
@@ -1,17 +1,19 @@
 package com.javarush.task.task04.task0435;
 
-/**
- * Циклом FOR вывести четные числа от 1 до 100
- * каждое с говой строки
- */
+/*
+Четные числа
+*/
+
 public class Solution {
     public static void main(String[] args) {
         //write this code
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 2; i <= 100; i+= 2) {
+            System.out.println(i);
+        }
+
+        /*for (int i = 1; i <= 100; i++) {
             if (i%2 == 0)
             System.out.println(i);
-
-        }
+        }*/
     }
 }
-   //   for (int i = 1; i <= 100; i+=2) {System.out.println(i);
