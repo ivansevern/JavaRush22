9846f040ed57ea7c997190fbf54f8afa9c529ab2
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-08 12:58:36 +0300

    add task0438

--- 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
@@ -1,15 +1,13 @@
 package com.javarush.task.task04.task0438;
 
-/**
- * Циклом For вывести
- * горизонтальную линию из 10 воссьмерок
- * вертикальнуюлинию из 10 восьмерок
- */
+/*
+Рисуем линии
+*/
+
 public class Solution {
     public static void main(String[] args) {
         //write this code
-      //  int a = 8;
-        for (int i = 1; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.print('8');
         }
         for (int i = 0; i <= 10; i++) {
