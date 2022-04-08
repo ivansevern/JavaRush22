2bce09f380020aa010123bf0cf44d71c36f71ba2
ivansevern <ivansevern@gmail.com>
2022-04-08 18:45:48 +0300

    add task0443

--- 1.JavaSyntax/src/com/javarush/task/task04/task0443/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0443/Solution.java
@@ -1,26 +1,22 @@
 package com.javarush.task.task04.task0443;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Ввести с клав строку name
- * Ввестис клав дату рождения (год, месяц, день)
- * Вывести
- * Меня зовут name
- * Я родился
- */
+/*
+Как назвали, так назвали
+*/
+
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String name = reader.readLine();
         String d = reader.readLine();
         String m = reader.readLine();
         String y = reader.readLine();
-        System.out.println("Меня зовут "+name+".");
-        System.out.println("Я родился " +d+ "." +m +"."+y);
+        System.out.println("Меня зовут " + name + ".");
+        System.out.println("Я родился " + d + "." + m + "." + y);
 
     }
 }
