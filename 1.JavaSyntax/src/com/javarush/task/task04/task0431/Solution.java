815cc55912aa41035dda60c4af50d95e52311c8a
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-08 12:11:10 +0300

    add task0431

--- 1.JavaSyntax/src/com/javarush/task/task04/task0431/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0431/Solution.java
@@ -1,13 +1,15 @@
 package com.javarush.task.task04.task0431;
 //TODO: Отсчет от 10 до 1 циком While
-/**
- * Отсчет от 10 до 1 циком While
- */
+
+/*
+От 10 до 1
+*/
+
 public class Solution {
     public static void main(String[] args) {
         //write this code
         int i = 10;
-        while (i>=1) {
+        while (i >= 1) {
             System.out.println(i);
             i--;
         }
