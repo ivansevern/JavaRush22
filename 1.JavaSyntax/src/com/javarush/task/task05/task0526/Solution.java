a3f3a2e2c113f39384584740c8223fe5a423e099
ivansevern <ivansevern@gmail.com>
2022-04-09 13:44:25 +0300

    add task0526

--- 1.JavaSyntax/src/com/javarush/task/task05/task0526/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0526/Solution.java
@@ -1,25 +1,20 @@
 package com.javarush.task.task05.task0526;
 
-/**
- * Внутри класса Solution создай public static Man и Woman
- * У классов должны быть поля:
- * name String, age int, address String
- * Создай конструкторы, в которые передаются все возможные параметры
- * Создай по 2 объекта каждого класса со всеми данными используя конструктор
- * Объекты выведи на экран в таком формате
- * name + "" + age + "" + asdress
- */
+/*
+Мужчина и женщина
+*/
+
 public class Solution {
     public static void main(String[] args) {
         //write this code
         Man man1 = new Man("Ivan", 37, "Kaluga");
-        Man man2 = new Man("Ivan", 38, "New-York");
-        System.out.println(man1.name + " " + man1.age + " " + man1.address);
-        System.out.println(man2.name + " " + man2.age + " " + man2.address);
+        Man man2 = new Man("Ivan", 39, "New-York");
         Woman woman1 = new Woman("1", 22, "22");
         Woman woman2 = new Woman("2", 23, "44");
-        System.out.println(woman1.name + " " + woman1.age + " " + woman1.address);
-        System.out.println(woman2.name + " " + woman2.age + " " + woman2.address);
+        System.out.println(man1);
+        System.out.println(man2);
+        System.out.println(woman1);
+        System.out.println(woman2);
 
     }
     //write this code
@@ -34,6 +29,10 @@ public class Solution {
             this.age = age;
             this.address = address;
         }
+
+        public String toString() {
+            return (name + " " + age + " " + address );
+        }
     }
 
     public static class Woman {
@@ -47,6 +46,9 @@ public class Solution {
             this.age = age;
             this.address = address;
         }
-    }
 
+        public String toString() {
+            return (name + " " + age + " " + address );
+        }
+    }
 }
