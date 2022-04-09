44b2b0ae65fcc8cb1a0ed8db0277eef48407e6b7
ivansevern <ivansevern@gmail.com>
2022-04-09 16:31:29 +0300

    add task0601

--- 1.JavaSyntax/src/com/javarush/task/task06/task0601/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0601/Cat.java
@@ -1,11 +1,13 @@
 package com.javarush.task.task06.task0601;
 
-/**В классе Cat создать метод protected void finalize() throws Throwable
- */
+/*
+Метод finalize класса Cat
+*/
+
 public class Cat {
     //write this code
     protected void finalize() throws Throwable {
-
+        super.finalize();
     }
     public static void main(String[] args) {
 
