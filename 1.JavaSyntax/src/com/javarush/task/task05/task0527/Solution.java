6a8f54f9bcd3fdfd5e2b241a1d6e01fc40eb19b7
ivansevern <ivansevern@gmail.com>
2022-04-09 13:57:34 +0300

    add task0527

--- 1.JavaSyntax/src/com/javarush/task/task05/task0527/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0527/Solution.java
@@ -1,55 +1,50 @@
 package com.javarush.task.task05.task0527;
-//TODO: создание объектов класса по конструктору
-/**
- * Создай классы Dog, Cat, Mouse
- * Добавь по 3 поля в каждый класс, на твой выбор
- * Создай объекты для героев мультика Том и Джери
- * Так много, как только вспомнишь
- */
+
+/*
+Том и Джерри
+*/
+
 public class Solution {
     public static void main(String[] args) {
-       Mouse jerryMouse = new Mouse("Jerry",12, 5);
        //write this code
-       Mouse mouse = new Mouse("Jerry", 3, 2);
-       Cat car = new Cat("Tom", 7, 4);
-       Dog dog = new Dog("Rax", 8, 2);
-    }
-
-    public static class Mouse {
-    String name;
-    int height;
-    int tall;
-
-        public Mouse(String name, int height, int tall) {
-            this.name = name;
-            this.height = height;
-            this.tall = tall;
-        }
-    }
-
-    //write this code
-    public static class Cat {
-        String name;
-        int height;
-        int tall;
-
-        public Cat(String name, int height, int tall) {
-            this.name = name;
-            this.height = height;
-            this.tall = tall;
-        }
+       new Mouse("Jerry", 12, 5);
+       new Dog("Rax", 12, 10);
+       new Cat("Tom", 15, 25);
     }
 
     public static class Dog {
         String name;
         int height;
-        int tall;
+        int tail;
 
-        public Dog(String name, int height, int tall) {
+        public Dog(String name, int height, int tail) {
             this.name = name;
             this.height = height;
-            this.tall = tall;
+            this.tail = tail;
         }
     }
 
+    public static class Cat {
+        String name;
+        int height;
+        int tail;
+
+        public Cat(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+
+    public static class Mouse {
+    String name;
+    int height;
+    int tail;
+
+        public Mouse(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
 }
