d957f0a729443fca889cb3a4c882ba79ae4b6f7e
ivansevern <ivansevern@gmail.com>
2022-04-09 09:12:14 +0300

    add task0505

--- 1.JavaSyntax/src/com/javarush/task/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0505/Solution.java
@@ -1,18 +1,15 @@
 package com.javarush.task.task05.task0505;
 
-/**
- * Создать 3 кота используя класс Сat
- * Провести 3 боя попарно между котами
- * Класс Сat создавать не надо.
- * Для боя использовать метод boolean fight(Cat anotherCat)
- * Результаты каждого боя вывести на экран с новойй строки
- */
+/*
+Кошачья бойня
+*/
+
 public class Solution {
     public static void main(String[] args) {
         //write this code
-        Cat cat1 = new Cat("cat1", 3, 3, 1);  //создаю кота по шаблону с параметрами
-        Cat cat2 = new Cat("cat2", 4, 4, 3);
-        Cat cat3 = new Cat("cat3", 5, 2, 4);
+        Cat cat1 = new Cat("Tom", 2, 2, 2);  //создаю кота по шаблону с параметрами
+        Cat cat2 = new Cat("Sam", 2, 2, 2);
+        Cat cat3 = new Cat("Fiona", 3, 3, 3);
 
         System.out.println(cat1.fight(cat2));                          //1 бой кота1 с котом2
         System.out.println(cat2.fight(cat3));
@@ -20,26 +17,27 @@ public class Solution {
     }
 
     public static class Cat {
+
         protected String name;
         protected int age;
         protected int weight;
-        protected int strenght;
+        protected int strength;
 
 
-        public Cat(String name, int age, int weight, int strenght) {
+        public Cat(String name, int age, int weight, int strength) {
             this.name = name;
             this.age = age;
             this.weight = weight;
-            this.strenght = strenght;
+            this.strength = strength;
         }
 
         public boolean fight(Cat anotherCat) {
             int ageCount = Integer.compare(this.age, anotherCat.age);
             int weightCount = Integer.compare(this.weight, anotherCat.weight);
-            int strenghtCount = Integer.compare(this.strenght, anotherCat.strenght);
+            int strengthCount = Integer.compare(this.strength, anotherCat.strength);
 
-            int count = ageCount + weightCount + strenghtCount;
-            return  count >0;  //return score >0 ? true : false;
+            int count = ageCount + weightCount + strengthCount;
+            return count > 0;  //return score >0 ? true : false;
         }
     }
 
