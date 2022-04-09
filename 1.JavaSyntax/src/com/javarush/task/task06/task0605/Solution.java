5db7493411c06dc5a05caf0b489aab9a077aee72
ivansevern <ivansevern@gmail.com>
2022-04-09 18:32:52 +0300

    add task0605

--- 1.JavaSyntax/src/com/javarush/task/task06/task0605/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0605/Solution.java
@@ -1,18 +1,12 @@
 package com.javarush.task.task06.task0605;
-/**
- * Программа должна считывать введенные пользователем вес в килограммах и рост в метрах и выводить на экран сообщение о индексе массы тела
- * Реализуй статический метод класса Body
- * Метод должен определить индекс массы тела, и вывести на экран сообщение
- * Недовес: меньше чем 18,5 - если индекс массы тела меньше 18.5
- * Нормальный: между 18.5 и 25 - если индекс массы тела между 18.5 и 25
- * Избыточный вес: между 25 и 30 - если индекс массы тела между 25 и 30
- * Ожирение: 30 или больше - если индекс массы тела больше 30
- * если индекс массы тела = вес в кг /(рост в метрах* рост в метрах)
- */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+/*
+Контролируем массу тела
+*/
 
 public class Solution {
     public static void main(String[] args) throws IOException {
@@ -26,21 +20,18 @@ public class Solution {
     }
 
     public static class Body {
-    public static void calculateMassIndex(double weight, double height) {
-        //write this code
-        double a = weight / (height * height);
-        if (a < 18.5)
-            System.out.println("Недовес: меньше чем 18.5");
-
-        if (a > 18.5 && a < 25)
-            System.out.println("Нормальный: между 18.5 и 25");
-
-        if (a > 25 && a < 30)
-            System.out.println("Избыточный вес: между 25 и 30");
-
-        if (a >= 30)
-            System.out.println("Ожирение: 30 или больше");
+        public static void calculateMassIndex(double weight, double height) {
+            //write this code
+            double index = weight / (height * height);
+            if (index < 18.5) {
+                System.out.println("Недовес: меньше чем 18.5");
+            } else if (index > 18.5 && index < 25) {
+                System.out.println("Нормальный: между 18.5 и 25");
+            } else if (index > 25 && index < 30) {
+                System.out.println("Избыточный вес: между 25 и 30");
+            } else if (index >= 30) {
+                System.out.println("Ожирение: 30 или больше");
+            }
+        }
     }
-    }
-
 }
