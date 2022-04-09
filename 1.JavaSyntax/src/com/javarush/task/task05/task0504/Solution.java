e67bd776034a39be5249623fe7cc9226eee05313
ivansevern <ivansevern@gmail.com>
2022-04-09 09:03:33 +0300

    add task0504

--- 1.JavaSyntax/src/com/javarush/task/task05/task0504/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0504/Solution.java
@@ -1,16 +1,15 @@
 package com.javarush.task.task05.task0504;
 //TODO: конструктор для класса
-/**
- * Создать 3 кота по шаблону
- * Наполнить котов данными
- */
+/*
+Трикотаж
+*/
 
 public class Solution {
     public static void main(String[] args) {
         //write this code
-        Cat cat1 = new Cat("cat1", 3, 2, 3); //создаю кота1 по шаблону класса с параметрами
-        Cat cat2 = new Cat("cat2", 2, 1, 2);
-        Cat cat3 = new Cat("cat3", 1, 4, 4);
+        Cat cat1 = new Cat("Tom", 2, 2, 2); //создаю кота1 по шаблону класса с параметрами
+        Cat cat2 = new Cat("Sam", 2, 2, 2);
+        Cat cat3 = new Cat("Fiona", 3, 3, 3);
 
     }
 
@@ -18,14 +17,13 @@ public class Solution {
         private String name;
         private int age;
         private int weight;
-        private int strenght;
+        private int strength;
 
-
-        public Cat(String name, int age, int weight, int strenght) {
+        public Cat(String name, int age, int weight, int strength) {
             this.name = name;
             this.age = age;
             this.weight = weight;
-            this.strenght = strenght;
+            this.strength = strength;
         }
     }
 }
