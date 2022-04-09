8f5ca2466a669df9d73ab117f470d51cd94ebb79
ivansevern <ivansevern@gmail.com>
2022-04-09 18:07:51 +0300

    add task0611

--- 1.JavaSyntax/src/com/javarush/task/task06/task0611/StringHelper.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0611/StringHelper.java
@@ -1,25 +1,21 @@
 package com.javarush.task.task06.task0611;
 
-/**
- * Сделать class StringHelper, у которого будут 2 статических метода
- * String multiply(String text) - возвращает строку повтореную 5 раз
- * String multiply(String text, int count) - возвращает строку повторенную count раз
- */
+/*
+Класс StringHelper
+*/
+
 public class StringHelper {
     public static String multiply(String text) {
-      //  String result = "";
         //write this code
         return multiply(text,5);
     }
 
     public static String multiply(String text, int count) {
-      //  String result = "";
         //write this code
        StringBuilder s = new StringBuilder();
         for (int i = 0; i < count; i++) {
             s.append(text);
         }
-
         return s.toString();
     }
 
