138f1622ca8773783b7bdfe29209691373969126
ivansevern <ivansevern@gmail.com>
2022-04-09 17:40:31 +0300

    add task0609

--- 1.JavaSyntax/src/com/javarush/task/task06/task0609/Util.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0609/Util.java
@@ -1,15 +1,15 @@
 package com.javarush.task.task06.task0609;
 
-/**
- * Реализовать метод double getDistance(int x1, int y1, int x2, int y2)
- * Он должен вычислять расстояния между точками
- * Используй метод double Math.sqrt(double a), который вычисляет корень квадратный переданного параметра
- */
+/*
+Расстояние между двумя точками
+*/
+
 public class Util {
     public static double getDistance(int x1, int y1, int x2, int y2) {
         //write this code
-        double a = Math.sqrt(((x2 - x1) * (x2 - x1) + ((y2 - y1) * (y2 - y1))));
-        return a;
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        return Math.sqrt(dx * dy + dx * dy);
     }
 
     public static void main(String[] args) {
