ba40efd6a75d52a8a53329c9791b6e3c45c66701
ivansevern <ivansevern@gmail.com>
2022-04-09 22:28:53 +0300

    add task0622

--- 1.JavaSyntax/src/com/javarush/task/task06/task0622/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0622/Solution.java
@@ -1,19 +1,7 @@
 package com.javarush.task.task06.task0622;
 
 /*
-Write a program that enters from the keyboard 5 numbers and displays them in an increasing order.
-input Example:
-3
-2
-15
-6
-17
-Output Example:
-2
-3
-6
-15
-17
+Числа по возрастанию
  */
 
 import java.io.BufferedReader;
@@ -22,10 +10,34 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)));
-
         // write this code
+        int firstNumber = Integer.parseInt(reader.readLine());
+        int secondNumber = Integer.parseInt(reader.readLine());
+        int thirdNumber = Integer.parseInt(reader.readLine());
+        int fourthNumber = Integer.parseInt(reader.readLine());
+        int fifthNumber = Integer.parseInt(reader.readLine());
 
-        int[] array = new int[5];
+        int[] array = new int[]{fifthNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber};
+        sort(array);
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+
+        /*int[] array = new int[5];
         for (int i =0; i < array.length; i++) {
             array[i] = Integer.parseInt(reader.readLine()); }
         for (int n = array.length-1; n>0; n--) {
@@ -34,13 +46,12 @@ public class Solution {
                     int sort = array[i];
                     array[i] = array[i+1];
                     array[i+1] = sort;
-
                 }
             }
         }
         for (int i:array) {
             System.out.println(i);
-        }
+        }*/
     }
 }
 
