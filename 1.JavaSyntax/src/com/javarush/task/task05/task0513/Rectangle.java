1dc8ca8e72eab6acede84230fa97cd4a8cdae115
ivansevern <ivansevern@gmail.com>
2022-04-09 10:30:01 +0300

    add task0513

--- 1.JavaSyntax/src/com/javarush/task/task05/task0513/Rectangle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0513/Rectangle.java
@@ -1,38 +1,12 @@
 package com.javarush.task.task05.task0513;
 
-/**
- * Создать class Rectangle
- * Его данными будут
- * top
- * left
- * width
- * height
- * Создать, как можно больше методов initialize
- */
+/*
+Собираем прямоугольник
+*/
+
 public class Rectangle {
     //write this code
     public int top, left, width, height;
-
-    public void initialize(int top) {
-        this.top = top;
-        this.left = 3;
-        this.width = 2;
-        this.height = 1;
-    }
-
-    public void initialize(int top, int left) {
-        this.top = top;
-        this.left = left;
-        this.width = 2;
-        this.height = 1;
-    }
-
-    public void initialize(int top, int left, int width) {
-        this.top = top;
-        this.left = left;
-        this.width = width;
-        this.height = 1;
-    }
 
     public void initialize(int top, int left, int width, int height) {
         this.top = top;
@@ -41,6 +15,26 @@ public class Rectangle {
         this.height = height;
     }
 
+    public void initialize(int top, int left) {
+        this.top = top;
+        this.left = left;
+        this.width = 0;
+        this.height = 0;
+    }
+
+    public void initialize(int top, int left, int width) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = width;
+    }
+
+    public void initialize(Rectangle rectangle) {
+        this.left = rectangle.left;
+        this.top = rectangle.top;
+        this.width = rectangle.width;
+        this.height = rectangle.height;
+    }
 
     public static void main(String[] args) {
 
