34c3b9c86781b1a2140215ed3914f84bd2069fbb
ivansevern <ivansevern@gmail.com>
2022-04-09 16:56:17 +0300

    add task0604

--- 1.JavaSyntax/src/com/javarush/task/task06/task0604/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0604/Cat.java
@@ -1,21 +1,30 @@
 package com.javarush.task.task06.task0604;
 
-/**
- * В конструкторе класса Cat[public Cat()]
- * увеличить счетчик котов на 1
- * В методе finalize уменьшать на 1
- */
+/*
+Ставим котов на счетчик
+*/
+
 public class Cat {
     public static int catCount = 0;
+
+    public String name;
+    public int age;
 
     public Cat() {
         catCount++;
     }
     //write this code
 
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    @Override
     protected void finalize() throws Throwable {
         catCount--;
+        super.finalize();
     }
+
     public static void main(String[] args) {
 
     }
