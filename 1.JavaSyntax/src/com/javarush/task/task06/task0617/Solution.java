571eae10bb2b016405d7bcd9706c112697b5d55e
ivansevern <ivansevern@gmail.com>
2022-04-09 20:27:33 +0300

    add task0617

--- 1.JavaSyntax/src/com/javarush/task/task06/task0617/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0617/Solution.java
@@ -1,23 +1,19 @@
 package com.javarush.task.task06.task0617;
 
-/**
- * В классе Solution создать public static класс Idea
- * В классе Idea создать метод public String getDescription(), который будет возвращать любую строку
- * В классе Solution создай статический метод public void printIdea(Idea idea), который будет выводить
- * на экран описание идеи - это то, что возвращает метод getDescription()
- */
+/*
+Блокнот для новых идей
+*/
+
 public class Solution {
     public static void main(String[] args) {
         printIdea(new Idea());
     }
-
-    //wwrite this code
+    //write this code
     public static void printIdea(Idea idea) {
         System.out.println(idea.getDescription());
     }
 
     public static class Idea {
-
         public String getDescription() {
             return "bla-bla-bla";
         }
