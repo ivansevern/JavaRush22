154ab2e270bd533408a3fd30fee594ffa7b227e7
ivansevern <ivansevern@gmail.com>
2022-04-09 13:29:03 +0300

    add task0525

--- 1.JavaSyntax/src/com/javarush/task/task05/task0525/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0525/Solution.java
@@ -1,18 +1,16 @@
 package com.javarush.task.task05.task0525;
 
-/**
- * По анологии с классом Duck создай class Cat и Dog
- * Подумай, что должен возвращать метод toString в классах Cat и Dog
- * В метод main создай по 2 объекта каждого класса и выведи их на экран
- * Объекты класса Duck уже созданы и выводятся на экран
- */
+/*
+И целой утки мало
+*/
+
 public class Solution {
     public static void main(String[] args) {
         Duck duck1 = new Duck();
         Duck duck2 = new Duck();
         System.out.println(duck1);
         System.out.println(duck2);
-        //wwrite thiscode
+        //write this code
         Cat cat1 = new Cat();
         Cat cat2 = new Cat();
         System.out.println(cat1);
