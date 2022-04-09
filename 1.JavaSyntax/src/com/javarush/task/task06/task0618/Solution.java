e6a96b882bb613a291fee15c95c40223ca44c321
ivansevern <ivansevern@gmail.com>
2022-04-09 21:33:24 +0300

    add task0618

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0618/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task06.task0618;
+
+/*
+KissMyShinnyMetalAss
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new KissMyShinnyMetalAss());
+    }
+
+    public static class KissMyShinnyMetalAss {
+    }
+}
