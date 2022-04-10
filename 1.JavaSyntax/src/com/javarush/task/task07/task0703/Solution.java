364db0662b79e7857942f9582120c9f7ff274e1f
ivansevern <ivansevern@gmail.com>
2022-04-10 14:00:38 +0300

    add task0703

--- 1.JavaSyntax/src/com/javarush/task/task07/task0703/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0703/Solution.java
@@ -1,29 +1,32 @@
 package com.javarush.task.task07.task0703;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * 1. Создать массив на 10 строк
- * 2. Создать массив на 10 чисел
- * 3. Ввести с клав 10 строк, заполнить ими массив строк
- * 4. В каждую ячейку массива чисел записать длину строки из массива строк, индекс/номер ячейки
- *    которой совпадает с текущим индексом из массива чисел
- * Вывести содержимое массива чисел на экран с новой строки каждое значение
- */
+/*
+Общение одиноких массивов
+*/
+
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = new String[10]; //1
-        int[] num = new int[10];       //2
-        for (int i = 0; i < 10; i++) { //3
+        String[] str = new String[10];
+        int[] numbers = new int[10];
+        for (int i = 0; i < 10; i++) {
+            str[i] = reader.readLine();
+            numbers[i] = str[i].length();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        /*for (int i = 0; i < 10; i++) {
             str[i] = reader.readLine();
         }
-        for (int i = 0; i < 10; i++) { //4
-            num[i] = str[i].length();
-            System.out.println(num[i]);
-        }
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = str[i].length();
+            System.out.println(numbers[i]);
+        }*/
     }
 }
