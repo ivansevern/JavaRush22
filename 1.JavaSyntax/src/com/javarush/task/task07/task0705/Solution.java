2221d0fb351ad559ca99137c85971d406d529eab
ivansevern <ivansevern@gmail.com>
2022-04-10 14:17:54 +0300

    add task0705

--- 1.JavaSyntax/src/com/javarush/task/task07/task0705/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0705/Solution.java
@@ -1,31 +1,42 @@
 package com.javarush.task.task07.task0705;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * 1. Создать массив на 20 чисел
- * 2. Ввести в него знач с клав
- * 3. Создать 2 массива на 10 чисел каждый
- * 4. Скопировать большой массив в 2 маленьких:
- *    половину чисел в 1 маленький, вторую во 2 маленький
- * 5. Вывести второй маленький массив на экран
- */
+/*
+Один большой массив и два маленьких
+*/
+
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int[] a = new int[20];  //1
-        int[] b = new int[10];  //3
-        int[] c = new int[10];  //3
-        for (int i = 0; i < 20; i++) {    //2
-            a[i] = Integer.parseInt(reader.readLine());
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
         }
-        for (int i = 0; i < 10; i++) {   //4
-            b[i] = a[i];
-            c[i] = a[i+10];
-            System.out.println(c[i]);    //5
+
+        int[] array1 = new int[10];
+        int[] array2 = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array1[i] = array[i];
+            array2[i] = array[i+10];
         }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(array2[i]);
+        }
+
+        /*int[] array = new int[20];
+        int[] b = new int[10];
+        int[] c = new int[10];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+        for (int i = 0; i < 10; i++) {
+            b[i] = array[i];
+            c[i] = array[i + 10];
+            System.out.println(c[i]);
+        }*/
     }
 }
