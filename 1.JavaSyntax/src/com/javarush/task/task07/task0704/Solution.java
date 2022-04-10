a222ba1d60bbea35dac445ab5cc69a9d04044f9e
ivansevern <ivansevern@gmail.com>
2022-04-10 14:06:36 +0300

    add task0704

--- 1.JavaSyntax/src/com/javarush/task/task07/task0704/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0704/Solution.java
@@ -1,23 +1,22 @@
 package com.javarush.task.task07.task0704;
 //TODO: заполнение массива и вывод в обратном порядке
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * 1. Создать массив на 10 чисел
- * 2. Ввести с клав 10 чисел и записать их в массив
- * 3. вывести на экран элементы массива в обратном порядке, каждое значение выводить с новой строки
- */
+/*
+Переверни массив
+*/
+
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //write this code
-        int[] num = new int[10];  //1
+        int[] num = new int[10];
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < 10; i++) {                    //2
+        for (int i = 0; i < 10; i++) {
             num[i] = Integer.parseInt(reader.readLine());
         }
-        for (int  i = num.length-1; i>=0; i--) {         //3
+        for (int i = num.length - 1; i >= 0; i--) {
             System.out.println(num[i]);
         }
     }
