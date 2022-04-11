19b73c61f4412adcac1a0afe951abcfd45059a9d
ivansevern <ivansevern@gmail.com>
2022-04-11 18:56:39 +0300

    add task0715

--- 1.JavaSyntax/src/com/javarush/task/task07/task0715/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0715/Solution.java
@@ -3,25 +3,21 @@ package com.javarush.task.task07.task0715;
 import java.util.ArrayList;
 
 /*
-1. Создай список слов:
-   мама,
-   мыла,
-   раму
-2. После каждого слова вставь в список строку, содержащую слово - именно
-3. Вывести результат на экран, каждый элемент списка с новой строки
+Продолжаем мыть раму
  */
+
 public class Solution {
     public static void main(String[] args) {
-        //wwrite this code
+        //write this code
         ArrayList<String> list = new ArrayList<>();
-        list.add("Mom");
-        list.add("wash");                   //1
-        list.add("window");
-        list.add(1, "exactly");                  //2
-        list.add(3, "exactly");
-        list.add(5, "exactly");
-        for (String s : list) {                               //2-3
-            System.out.println(s);                            //3
+        list.add("мама");
+        list.add("мыла");
+        list.add("раму");
+        list.add(1, "именно");
+        list.add(3, "именно");
+        list.add(5, "именно");
+        for (String s : list) {
+            System.out.println(s);
         }
     }
 }
