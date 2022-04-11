5d46926d007f264e73fea175d5f0adc3a12454ee
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-11 12:46:48 +0300

    add task0711

--- 1.JavaSyntax/src/com/javarush/task/task07/task0711/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0711/Solution.java
@@ -1,31 +1,42 @@
 package com.javarush.task.task07.task0711;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /*
-1. Создать список строк
-2. Добавить туда 5 строк с клав
-3. Выполнить 13 раз: удалить последнюю строку и вставить ее в начало
-4. Циклом вывести содержимое результирующего списка на экран
+Удалить и вставить.
  */
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Object> list = new ArrayList<>();             //1
+        ArrayList<String> strings = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            list.add(reader.readLine());                        //2
+            String string = reader.readLine();
+            strings.add(string);
         }
-        for (int i = 0; i < 13; i++) {                          //3
+
+        for (int i = 0; i < 13; i++) {
+            String string = strings.remove(4);
+            strings.add(0, string);
+        }
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        /*ArrayList<Object> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(reader.readLine());
+        }
+        for (int i = 0; i < 13; i++) {
 
            String l = (String) list.remove(4);
             list.add(0, l);
         }
-        for (Object r : list) {                                //4
+        for (Object r : list) {
             System.out.println(r);
-        }
+        }*/
     }
 }
