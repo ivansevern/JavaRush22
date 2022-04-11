8bd93842b5c897ce9df1960fba794e00e603d10a
ivansevern <ivansevern@gmail.com>
2022-04-11 19:41:23 +0300

    add task0718

--- 1.JavaSyntax/src/com/javarush/task/task07/task0718/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0718/Solution.java
@@ -6,21 +6,24 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /*
-1. Ввели с клав 10 слов в список строк
-2. Определить, является ли список упорядоченным по возврастанию длины строки
-3. В случае отрицательного ответа вывести на экран индекс 1 элемента, нарушающего такую упорядоченность
+Проверка на упорядоченность
  */
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> strings = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            list.add(reader.readLine());                                  //1
+            String string = reader.readLine();
+            strings.add(string);
         }
-        for (int i = 0; i < list.size()-1;  i++) {                        //2
-            if (list.get(i).length() > list.get(i + 1).length()) {
-                System.out.println(i+1);                                  //3
+        String bugString = strings.get(0);
+        for (int i = 0; i < 10;  i++) {
+            if (strings.get(i).length() > bugString.length()) {
+                bugString = strings.get(i);
+            } else{
+                System.out.println(i);
                 break;
             }
         }
