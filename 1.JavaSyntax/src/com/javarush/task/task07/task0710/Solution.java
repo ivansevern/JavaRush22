fb426d3488c39531da69428eb5951e4c6a0f70ed
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-11 12:39:05 +0300

    add task0710

--- 1.JavaSyntax/src/com/javarush/task/task07/task0710/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0710/Solution.java
@@ -5,20 +5,22 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /*
-1. Создай список строк
-2. Добавь в него 10 строк с клав, но добавлять не в конец, а в начало
-3. Циклом вывести содержимое на экран
+В начало списка
  */
 public class Solution {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         //write this code
-        ArrayList<String> list = new ArrayList<>();                                     //1
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));   
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < 10; i++) {
-            list.add(0, reader.readLine());                                       //2
+            list.add(0, reader.readLine());
         }
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(list.get(i));
+
+        /*for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }*/
         }
     }
 }
