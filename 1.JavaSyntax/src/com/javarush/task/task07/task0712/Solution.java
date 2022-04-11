70aa6cef8f925697234b77cb08d17afbbb02f0d1
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-11 12:58:43 +0300

    add task0712

--- 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
@@ -6,38 +6,61 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /*
-1. Создай список строк
-2. Добавь в него 10 строк
-3. Узнай, какая строка в списке встретится раньше:
-   самаяя короткая или самая длинная. Если несколько,то самая первая
-4. Выведи на экран строку строку из п.3
+Самые-самые
  */
+
 public class Solution {
     public static void main(String[] args) throws Exception{
         //write this code
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> list = new ArrayList<String>();                  //1
+        ArrayList<String> strings = new ArrayList<String>();
+
         for (int i = 0; i < 10; i++) {
-            list.add(reader.readLine());                                   //2
+            String string = reader.readLine();
+            strings.add(string);
+        }
+
+        String minSizeElement = strings.get(0);
+        int posMinElement = 0;
+        String maxSizeElement = strings.get(0);
+        int posMaxElement = 0;
+        for (int i = 0; i < 10; i++) {
+            if (strings.get(i).length() < minSizeElement.length()) {
+                minSizeElement = strings.get(i);
+                posMinElement = i;
+            } else if (strings.get(i).length() > maxSizeElement.length()) {
+                maxSizeElement = strings.get(i);
+                posMaxElement = i;
+            }
+        }
+
+        if (posMinElement < posMaxElement) {
+            System.out.println(minSizeElement);
+        } else {
+            System.out.println(maxSizeElement);
+        }
+
+        /*for (int i = 0; i < 10; i++) {
+            strings.add(reader.readLine());
         }
         ArrayList<Integer> list1 = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
-            list1.add(list.get(i).length());
+            list1.add(strings.get(i).length());
         }
-        Collections.sort(list1);                                            //3
+        Collections.sort(list1);
         int min = list1.get(0);
         int max = list1.get(list1.size() - 1);
-        for (int i = 0; i < 10; i++) {                                      //4
-            if (list.get(i).length() == min) {
-                System.out.println(list.get(i));
+        for (int i = 0; i < 10; i++) {
+            if (strings.get(i).length() == min) {
+                System.out.println(strings.get(i));
                 break;
             }
-            else if (list.get(i).length() == max){                          //4
-                System.out.println(list.get(i));
+            else if (strings.get(i).length() == max){
+                System.out.println(strings.get(i));
                 break;
             }
-        }
+        }*/
     }
 
 }
