b0a6ef5bd21ff9f936fb2633ac3c657c7efc73f1
ivansevern <ivansevern@gmail.com>
2022-04-11 19:46:34 +0300

    add task0719

--- 1.JavaSyntax/src/com/javarush/task/task07/task0719/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0719/Solution.java
@@ -1,26 +1,24 @@
 package com.javarush.task.task07.task0719;
 
-/*
-1. Ввести с клав 10 чисел и заполнить ими список
-2. Вывести их в обратном порядке. Каждый элемент с новой строки
- Использовать только цикл for
- */
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+/*
+Вывести числа в обратном порядке
+ */
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         //write this code
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            list.add(Integer.parseInt(reader.readLine()));           //1
+            numbers.add(Integer.parseInt(reader.readLine()));
         }
-        for (int i = list.size()-1; i >=0 ; i--) {                   //2
-            System.out.println(list.get(i));
+        for (int i = numbers.size()-1; i >=0 ; i--) {
+            System.out.println(numbers.get(i));
         }
     }
 }
