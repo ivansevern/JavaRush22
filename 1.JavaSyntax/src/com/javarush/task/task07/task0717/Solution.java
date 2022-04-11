6fb0699f730d4ac8c06c1d98da4494e76c7b2ad3
ivansevern <ivansevern@gmail.com>
2022-04-11 19:32:37 +0300

    add task0717

--- 1.JavaSyntax/src/com/javarush/task/task07/task0717/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0717/Solution.java
@@ -1,39 +1,38 @@
 package com.javarush.task.task07.task0717;
 
-/*
-1. Введи с клав 10 слов в список
-2. Метод doubleValues должен удваивать слова по принципу:
-   альфа, бета, гамма
-   альфа, альфа, бета, бета, гамма, гамма
-3. Выведи результат на экран, каждое значение с новой строки
- */
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+/*
+Удваиваем слова
+ */
+
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         //read strings c консоли и объявить ArrayList тут
-        ArrayList<String> list = new ArrayList<String>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> strings = new ArrayList<String>();
         for (int i = 0; i < 10; i++) {
-            list.add(reader.readLine());                                                   //1
+            String string = reader.readLine();
+            strings.add(string);
         }
 
-        ArrayList<String> result = doubleValues(list);
+        ArrayList<String> result = doubleValues(strings);
         //вывести на экран result
-       for (String a : list) {                                                    //с новой строки каждое слово
-            System.out.println(a);                                                //3
+        for (String string : result) {
+            System.out.println(string);
         }
     }
 
     public static ArrayList<String> doubleValues(ArrayList<String> list) {
         //write this code
-        for (int i = list.size()-1; i>=0; i--) {                                        //2
-            list.add(i, list.get(i));
-
+        ArrayList<String> result = new ArrayList<>();
+        for (String string : list) {
+            result.add(string);
+            result.add(string);
         }
-        return list;
+        return result;
     }
 }
