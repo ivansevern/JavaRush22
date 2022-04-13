5dbcee54ce8ea068a441802ed38237a3361d61c0
ivansevern <ivansevern@gmail.com>
2022-04-13 18:32:22 +0300

    add task0803

--- 1.JavaSyntax/src/com/javarush/task/task08/task0803/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0803/Solution.java
@@ -4,5 +4,39 @@ package com.javarush.task.task08.task0803;
 Коллекция Map из котов
 */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    public static void main(String[] args) throws Exception {
+        String[] cats = new String[]{"васька", "мурка", "дымка", "рыжик", "серый", "снежок", "босс", "борис", "визя", "гарри"};
+
+        Map<String, Cat> map = addCatsToMap(cats);
+
+        for (Map.Entry<String, Cat> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " - " + pair.getValue());
+        }
+    }
+
+    public static Map<String, Cat> addCatsToMap(String[] cats) {
+        //напишите тут ваш код
+        Map<String, Cat> map = new HashMap<>();
+        for (String name : cats) {
+            map.put(name, new Cat(name));
+        }
+        return  map;
+    }
+
+    public static class Cat {
+    String name;
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name != null ? name.toUpperCase() : null;
+        }
+    }
 }
