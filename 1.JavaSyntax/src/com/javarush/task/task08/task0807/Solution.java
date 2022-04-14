b601e6dcb046f376bc109d009969762821b7fa32
ivansevern <ivansevern@gmail.com>
2022-04-14 20:06:20 +0300

    add task0807

--- 1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
@@ -4,15 +4,18 @@ package com.javarush.task.task08.task0807;
 LinkedList и ArrayList
 */
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Solution {
     public static Object createArrayList() {
         //напишите тут ваш код
-
+        return new ArrayList<>();
     }
 
     public static Object createLinkedList() {
         //напишите тут ваш код
-
+        return new LinkedList<>();
     }
 
     public static void main(String[] args) {
