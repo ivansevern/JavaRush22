4f2621fb86e103963731d4b874ebd446bc7173d6
ivansevern <ivansevern@gmail.com>
2022-04-16 09:52:56 +0300

    add task0811

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0811/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task08.task0811;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+Квартет методы
+*/
+
+public class Solution {
+    public static List getListForGet() {
+        //напишите тут ваш код
+
+       /* ArrayList a = new ArrayList<>();
+        return a;*/
+
+        return new ArrayList();
+    }
+
+    public static List getListForSet() {
+        //напишите тут ваш код
+
+       /* ArrayList a = new ArrayList<>();
+        return a;*/
+
+        return new ArrayList();
+    }
+
+    public static List getListForAddOrInsert() {
+        //напишите тут ваш код
+
+        /*LinkedList a = new LinkedList<>();
+        return  a;*/
+
+        return new LinkedList();
+    }
+
+    public static List getListForRemove() {
+        //напишите тут ваш код
+
+        /*LinkedList a = new LinkedList<>();
+        return  a;*/
+
+        return new LinkedList();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
