2b92dd9b044e58660af133a0a697f137a6b5ab32
ivansevern <ivansevern@gmail.com>
2022-04-17 14:23:00 +0300

    add task 0903

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0903/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task09.task0903;
+
+/*
+Кто меня вызывал?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        method1();
+    }
+
+    public static int method1() {
+        method2();
+        //напишите тут ваш код
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method2() {
+        method3();
+        //напишите тут ваш код
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method3() {
+        method4();
+        //напишите тут ваш код
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method4() {
+        method5();
+        //напишите тут ваш код
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method5() {
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+}
