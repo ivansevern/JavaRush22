21b295f7390d610bf3697d3e8363f8939381a831
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-18 12:14:44 +0300

    add task0907

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0907/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task09.task0907;
+
+/*
+Исключение при работе с числами I Java Syntax: 9 уровень, 6 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        try {
+            int a = 42 / 0;
+        //напишите тут ваш код
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+    }
+}
