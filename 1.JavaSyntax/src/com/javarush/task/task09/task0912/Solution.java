4e6822effaf9808a786052dedc8af62aaf8bb40a
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-18 12:34:45 +0300

    add task0912

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0912/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task09.task0912;
+
+/*
+Исключение при работе с числами I Java Syntax: 9 уровень, 6 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        try {
+            int num = Integer.parseInt("XYZ");
+            System.out.println(num);
+            //напишите тут ваш код
+        } catch (NumberFormatException e) {
+            System.out.println(e.getClass().getName());
+        }
+    }
+}
