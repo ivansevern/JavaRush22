8e7245571ff8f484a76b5d664b524e3e51b168db
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-21 12:52:52 +0300

    add task1013

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1013/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task10.task1013;
+
+/*
+Конструкторы класса Human
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public static class Human {
+        //напишите тут ваши переменные и конструкторы
+        private String name;
+        private int age;
+        private int height;
+        private int weight;
+        private Human mother;
+        private Human father;
+
+        public Human() {
+        }
+
+        public Human(String name) {
+            this.name = name;
+        }
+
+        public Human(String name, int height) {
+            this.name = name;
+            this.height = height;
+        }
+
+        public Human(String name, int height, int weight) {
+            this.name = name;
+            this.height = height;
+            this.weight = weight;
+        }
+
+        public Human(String name, int age, int height, int weight, Human mother) {
+            this.name = name;
+            this.age = age;
+            this.height = height;
+            this.weight = weight;
+            this.mother = mother;
+        }
+
+        public Human(int height, int weight) {
+            this.height = height;
+            this.weight = weight;
+        }
+
+        public Human(int age, int height, int weight) {
+            this.age = age;
+            this.height = height;
+            this.weight = weight;
+        }
+
+        public Human(String name, Human mother, Human father) {
+            this.name = name;
+            this.mother = mother;
+            this.father = father;
+        }
+
+        public Human(Human mother, Human father) {
+            this.mother = mother;
+            this.father = father;
+        }
+    }
+}
