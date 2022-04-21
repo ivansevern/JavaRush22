f36d872a77735646b06a293150523707704acf49
ivansevern <ivansevern@gmail.com>
2022-04-21 20:51:54 +0300

    add task1110

--- 2.JavaCore/src/com/javarush/task/task11/task1110/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task11/task1110/Solution.java
@@ -1,7 +1,7 @@
 package com.javarush.task.task11.task1110;
 
 /*
-* Добавить недостающие поля
+Не забываем инкапсулировать
 */
 
 public class Solution {
@@ -9,30 +9,30 @@ public class Solution {
     }
 
     public class Cat {
-    private String name;
-    private int age;
-    private int weight;
-    private int speed;
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
 
-        public Cat(String name, final int age, final int weight) {
+        public Cat(String name, int age, int weight) {
             this.name = name;
             this.age = age;
             this.weight = weight;
         }
 
         public String getName() {
-            return this.name;
+            return name;
         }
 
         public int getAge() {
-            return this.age;
+            return age;
         }
 
-        public void setWeight(final int weight) {
+        public void setWeight(int weight) {
             this.weight = weight;
         }
 
-        public void setSpeed(final int speed) {
+        public void setSpeed(int speed) {
             this.speed = speed;
         }
     }
