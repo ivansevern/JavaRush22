07f99527039296429d67adc50ef7aac4a808d15b
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-22 12:32:21 +0300

    add task1213

--- 2.JavaCore/src/com/javarush/task/task12/task1213/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1213/Solution.java
@@ -1,18 +1,18 @@
 package com.javarush.task.task12.task1213;
 
 /*
- * Исправить код
+"Исправить код" часть 2
  */
 
 public class Solution {
     public static void main(String[] args) {
     }
 
-    public static abstract class Pet {
+    public static abstract class Pet { //public static class -> public static abstract class
         public String getName() {
             return "Я - котенок";
         }
 
-        public abstract Pet getChild();
+        public abstract Pet getChild(); //public Pet -> public abstract Pet
     }
 }
