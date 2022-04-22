1014476e4ed2eb7a3f83a43118230b31583a2cee
ivansevern <ivansevern@gmail.com>
2022-04-22 19:21:59 +0300

    add task1309

--- 2.JavaCore/src/com/javarush/task/task13/task1309/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1309/Solution.java
@@ -1,15 +1,11 @@
 package com.javarush.task.task13.task1309;
 
 /*
-* 1. Создать интерфейс CanMove с методом speed
-* 2. Сделать так, чтобы speed возвращал значение типа Double и ничего не принимал в качестве аргументов
-* 3. Создать и унаследовать интерфейс CanFly  от интерфейса CanMove от интерфейса CanMove
-* 4. Добавить в интерфейс CanFly метод speed
-* 5. Убедиться, что speed возвращает значение типа Double и принимает один параметр типа CanFly
+Все, что движется
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
     }
 
     interface CanMove {
