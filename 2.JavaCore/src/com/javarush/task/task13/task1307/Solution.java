1439d3ec53b32c2f08b6bac33f28a4b1089e43c8
ivansevern <ivansevern@gmail.com>
2022-04-22 18:31:25 +0300

    add task1307

--- 2.JavaCore/src/com/javarush/task/task13/task1307/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1307/Solution.java
@@ -1,18 +1,18 @@
 package com.javarush.task.task13.task1307;
 
 /*
-* В классе StringObject реализуй интерфейс SimpleObject  с параметрами типа String
+Параметризованый интерфейс
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
     }
 
     interface SimpleObject<T> {
         SimpleObject<T> getInstance();
     }
 
-    class StringObject implements SimpleObject<String> {
+    class StringObject implements SimpleObject<String> { //
         @Override
         public SimpleObject<String> getInstance() {
             return null;
