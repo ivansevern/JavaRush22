0a523d1b20c3e807672f0aa239ab2845e4dc6870
ivansevern <ivansevern@gmail.com>
2022-04-22 18:08:35 +0300

    add task1305

--- 2.JavaCore/src/com/javarush/task/task13/task1305/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1305/Solution.java
@@ -1,12 +1,12 @@
 package com.javarush.task.task13.task1305;
 
 /*
-*
+Четыре ошибки
 */
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(Dream.HOBBY.toString());
+        System.out.println(Dream.HOBBY.toString()); //1 new Dream.HOBBY.toString() -> Dream.HOBBY.toString()
         System.out.println(new Hobby().toString());
     }
 
@@ -14,10 +14,11 @@ public class Solution {
     }
 
     interface Dream {
+//        private static hobby HOBBY = new Hobby();  //2
         Hobby HOBBY = new Hobby();
     }
 
-    static class Hobby implements Desire, Dream {
+    static class Hobby implements Desire, Dream {  //3,4 class Hobby implements Dream -> static class Hobby implements Desire, Dream
         static int INDEX = 1;
 
         @Override
