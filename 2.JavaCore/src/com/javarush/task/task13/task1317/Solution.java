730d6134ef9fb6c30fa4bb106592e08da9a9f63b
ivansevern <ivansevern@gmail.com>
2022-04-23 22:15:29 +0300

    add task1317

--- 2.JavaCore/src/com/javarush/task/task13/task1317/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1317/Solution.java
@@ -1,17 +1,17 @@
 package com.javarush.task.task13.task1317;
 
 /*
-* Реализуй в классе Today интерфейс Weather
+The weather is fine
 */
 
 public class Solution {
-    public static void main(String[] args) {
-       /* System.out.println(new Today(WeatherType.CLOUDY));
+   /* public static void main(String[] args) {
+        System.out.println(new Today(WeatherType.CLOUDY));
         System.out.println(new Today(WeatherType.FOGGY));
         System.out.println(new Today(WeatherType.FREEZING));
     }
 
-    static class Today implements Weather {
+    static class Today implements Weather { //static class Today -> static class Today implements Weather
         private String type;
 
         Today(String type) {
@@ -25,6 +25,6 @@ public class Solution {
         @Override
         public String toString() {
             return String.format("Today it will be %s", this.getWeatherType());
-        }*/
-    }
+        }
+    }*/
 }
