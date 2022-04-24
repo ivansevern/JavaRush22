b7ea5a07d5106e26f5dfa4df3ed4eac9a19762af
ivansevern <ivansevern@gmail.com>
2022-04-24 10:08:12 +0300

    add task1402

--- 2.JavaCore/src/com/javarush/task/task14/task1402/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1402/Solution.java
@@ -1,12 +1,19 @@
-package task14.task1402;
+package com.javarush.task.task14.task1402;
+
+/*
+Bingo-2!
+*/
 
 public class Solution {
     public static void main(String[] args) {
+//        Cat cat = new Cat();
         Cat cat = new TomCat();
 
         boolean isCat = cat instanceof Cat;
         boolean isMovable = cat instanceof CanMove;
         boolean isTom = cat instanceof TomCat;
+
+        if (isCat && isMovable && isTom) System.out.println("Bingo!");
     }
 
     interface CanMove {
