df1774daea8591954c238b35a7ca3ca3e7a3b7ff
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-25 12:53:32 +0300

    add task1415

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1415/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task14.task1415;
+
+/*
+Клининговый центр
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Apartment> apartments = new ArrayList<>();
+        apartments.add(new OneRoomApt());
+        apartments.add(new TwoRoomApt());
+        apartments.add(new ThreeRoomApt());
+    }
+
+    public static void cleanAllApartments(List<Apartment> apartments) {
+        for (Apartment apartment : apartments) {
+            if (apartment instanceof OneRoomApt) ((OneRoomApt) apartment).clean1Room();
+            else if (apartment instanceof TwoRoomApt) ((TwoRoomApt) apartment).clean2Room();
+            else if (apartment instanceof ThreeRoomApt) ((ThreeRoomApt) apartment).clean3Room();
+        }
+    }
+
+    interface Apartment {
+    }
+
+    static class OneRoomApt implements Apartment {
+        void clean1Room() {
+            System.out.println("1 rooms are cleaned");
+        }
+    }
+
+    static class TwoRoomApt implements Apartment {
+        void clean2Room() {
+            System.out.println("2 rooms are cleaned");
+        }
+    }
+
+    static class ThreeRoomApt implements Apartment {
+        void clean3Room() {
+            System.out.println("3 rooms are cleaned");
+        }
+    }
+}
