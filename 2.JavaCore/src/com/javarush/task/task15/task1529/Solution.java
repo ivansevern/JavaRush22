b385f859d1387bd60cf74aec79695722bafdc6a4
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-28 11:03:04 +0300

    add task 1529

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task15.task1529;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static {
+        //add your code here
+        reset();
+    }
+
+    public static CanFly result;
+
+    public static void reset() throws IllegalArgumentException { //throws IllegalArgumentException
+        //add your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String key = reader.readLine();
+            if ("plane".equals(key)) {
+                key = reader.readLine();
+                result = new Plane(Integer.parseInt(key));
+            } else if ("helicopter".equals(key)) {
+                result = new Plane(Integer.parseInt(key));
+                result = new Helicopter();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
