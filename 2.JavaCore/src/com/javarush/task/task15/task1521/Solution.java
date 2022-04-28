b81c930694748b35658a3d4ed09d3e09962357a6
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-28 13:10:58 +0300

    add task 1521

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1521/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task15.task1521;
+
+import java.math.BigDecimal;
+
+/*
+ООП. Перегрузка
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //Блок2
+        //Вызов для Object
+       /* new Tree().info((Object) new Integer("4"));
+        new Tree().info((Object) new Short("4"));
+        new Tree().info((Object) new BigDecimal("4"));
+
+        //Блок3
+        //Вызов для Number
+        new Tree().info((Object) new Integer("4"));
+        new Tree().info((Object) new Short("4"));
+        new Tree().info((Object) new BigDecimal("4"));
+
+        //Блок4
+        //Вызов для String
+        new Tree().info((Object) new Integer("4").toString());
+        new Tree().info((Object) new Short("4").toString());
+        new Tree().info((Object) new BigDecimal("4").toString());*/
+    }
+}
