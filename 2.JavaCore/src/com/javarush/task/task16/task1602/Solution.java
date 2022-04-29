459f0a7bc7cbabf9d2fa3e4faa5d902287905e21
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-29 09:24:18 +0300

    add task1602

--- 2.JavaCore/src/com/javarush/task/task16/task1602/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1602/Solution.java
@@ -1,23 +1,22 @@
 package com.javarush.task.task16.task1602;
 
 /*
-* 1. Создать public static class TestThread, унаследованный от класса Thread
-* 2. Создать статик блок внутри TestThread, который выводить в консоль It's a static block u=inside TestThread
-* 3. Метод Run должен выводить в консоль It's a run method
+My second thread
 */
 
 public class Solution {
     public static void main(String[] args) {
-
+        TestThread thread = new TestThread();
+        thread.start();
     }
 
     public static class TestThread extends Thread {
         static {
-            System.out.println("It's a static block u=inside TestThread");
+            System.out.println("it's a static block inside TestThread");
         }
 
         public void run() {
-            System.out.println("It's a run method");
+            System.out.println("it's a run method");
         }
     }
 }
