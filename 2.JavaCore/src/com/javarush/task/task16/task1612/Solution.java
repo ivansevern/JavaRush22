2c664fb94c24cbcac83898d5c8e3be2848b27734
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-04-29 11:36:59 +0300

    add task1612

--- 2.JavaCore/src/com/javarush/task/task16/task1612/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1612/Solution.java
@@ -1,20 +1,23 @@
 package com.javarush.task.task16.task1612;
 
 /*
-* 1. Разберись, что делает программа
-* 2. Реализуй логику метода doStep так, чтобы учитывалась скорость бегуна
-* 2.1 Метод getSpeed() в классе Runner показывает, сколько шагов в секунду делает бегун
-* Нужно, чтобы бегун действительно делал заданное количество шагов в секунду
-* Если Иванов делает 4 шага в секунду, то за 2 сек он сделает 8 шагов
-* Если Петров делает 2 шага в сек, то за 2 сек он сделает 4 шага
-* 2.2 Метод sleep в классе Thread принимает параметр типа long
+Stopwatch (Секундомер)
 */
 
 public class Solution {
     public static volatile boolean isStopped = false;
 
     public static void main(String[] args) throws InterruptedException{
-
+        Runner ivanov = new Runner("Ivanov", 4);
+        Runner petrov = new Runner("petrov", 2);
+        //на старт
+        //внимание
+        //марш
+        ivanov.start();
+        petrov.start();
+        Thread.sleep(2000);
+        isStopped = true;
+        Thread.sleep(1000);
     }
 
     public static class Stopwatch extends Thread {
@@ -36,7 +39,7 @@ public class Solution {
 
         private void doStep() throws InterruptedException {
             stepNumber++;
-            //
+            //add your code here
             Thread.sleep(1000 / owner.getSpeed());
             System.out.println(owner.getName() + " делает шаг №" + stepNumber + "!");
         }
