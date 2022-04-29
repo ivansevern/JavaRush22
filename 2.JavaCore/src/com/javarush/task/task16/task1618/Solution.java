02229e8481c2966610a288f92dbf71a97de38e2e
ivansevern <ivansevern@gmail.com>
2022-04-29 21:38:16 +0300

    add task1618

--- 2.JavaCore/src/com/javarush/task/task16/task1618/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1618/Solution.java
@@ -1,17 +1,18 @@
 package com.javarush.task.task16.task1618;
 
 /*
- * Создай нить TestThread
- * В методе main создай экземпляр нити, запусти, а потом прерви ее используя метод interrupt()
+Снова interrupt
  */
 
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
+        //add your code here
         TestThread t = new TestThread();
         t.start();
         t.interrupt();
     }
 
+    //add your code below
     public static class TestThread extends Thread {
         public void run() {
         }
