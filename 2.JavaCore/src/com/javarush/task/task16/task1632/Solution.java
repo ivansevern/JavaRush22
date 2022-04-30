32cb414fa9019dbc027a474c1895c683d03a795b
ivansevern <ivansevern@gmail.com>
2022-04-30 09:45:29 +0300

    add task1632

--- 2.JavaCore/src/com/javarush/task/task16/task1632/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1632/Solution.java
@@ -1,21 +1,14 @@
 package com.javarush.task.task16.task1632;
 
-/*
- * 1. Создай 5 различных своих нитей (наследников Thread)
- * 1.1 Нить 1 должна бесконечно выполняться
- * 1.2 Нить 2 должна выводить InterruptedException
- * 1.3 Нить 3 должна каждые полсекунды выводить ура
- * 1.4 Нить 4 должна реализовать интерфейс Message, при вызове метода showWarning нить должна останавливаься
- * 1.5 Нить 5 должна читать с консоли числа пока не введено слово N, а потом вывести в консоль сумму введеных чисел
- * 2. В статическом блоке добавь свои нити в List<Thread>threads в перечисленном порядке
- * 3. Нити не должны стартовать автоматически
- */
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+
+/*
+Клубок
+ */
 
 public class Solution {
     public static List<Thread> threads = new ArrayList<>(5);
