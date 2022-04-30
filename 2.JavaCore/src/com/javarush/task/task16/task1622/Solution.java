41277ea311f8b34da170c763f9f262205fd8bda8
ivansevern <ivansevern@gmail.com>
2022-04-30 10:17:46 +0300

    add task1622

--- 2.JavaCore/src/com/javarush/task/task16/task1622/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1622/Solution.java
@@ -1,10 +1,7 @@
 package com.javarush.task.task16.task1622;
 
 /*
-* 1. В методе run после всех действий поставь задержку в 10 миллисекунд.
-* Выведи Нить прервана, если нить будет прервана
-* 2. Сделай так, чтобы все нити выполнялись последовательно: сначала для нити №1 отсчет с COUNT до 1,
-* потом для нити №2 с COUNT до 1 и т.д.
+Последовательность выполнения нитей I Java Core: 6 уровень, 13 лекция
 */
 
 public class Solution {
@@ -12,7 +9,8 @@ public class Solution {
 
     public static void main(String[] args) throws InterruptedException{
         for (int i = 0; i < COUNT; i++) {
-            new SleepingThread().join();
+            new SleepingThread().join(); //new SleepingThread() -> new SleepingThread().join()
+            //write your code here
         }
     }
 
@@ -30,6 +28,7 @@ public class Solution {
                 System.out.println(this);
 
                 if (--countdownIndex == 0) return;
+                //write your code here
 
                 try {
                     Thread.sleep(10);
@@ -42,10 +41,7 @@ public class Solution {
 
 
         public String toString() {
-            return "#" +
-                    getName() +
-                    ": " +
-                    countdownIndex;
+            return "#" + getName() + ": " + countdownIndex;
         }
     }
 }
