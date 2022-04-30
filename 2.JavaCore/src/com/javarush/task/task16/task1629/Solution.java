82484e4c17498e55e71a9a55e9fd0aa602797f73
ivansevern <ivansevern@gmail.com>
2022-04-30 11:48:57 +0300

    add task1629

--- 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
@@ -1,16 +1,12 @@
 package com.javarush.task.task16.task1629;
 
-/*
-* 1. В классе Solution создать public static класс нити Read3Strings унаследовавшись от Thread
-* 2. В методе run реализовать чтение с консоли трех строк
-* 3. Три подряд введеных строки должны считываться с в одной нити и объединиться в одну строку через пробел
-* 4. В методе main вывести результат для каждой нити
-* 5. Используй join
-*/
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+/*
+Только по-очереди!
+*/
 
 public class Solution {
     public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -18,6 +14,7 @@ public class Solution {
     public static void main(String[] args) throws InterruptedException{
         Read3Strings t1 = new Read3Strings();
         Read3Strings t2 = new Read3Strings();
+        //add your code here
         t1.start();
         t1.join();
         t2.start();
@@ -25,6 +22,7 @@ public class Solution {
         t1.printResult();
         t2.printResult();
     }
+    //add your code here
 
     public static class Read3Strings extends Thread {
         public String result = "";
@@ -44,5 +42,4 @@ public class Solution {
             System.out.println(result);
         }
     }
-
 }
