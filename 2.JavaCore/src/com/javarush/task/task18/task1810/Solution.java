d9aaa3c1e674ebd25572c0d2c58e7852a61ffab3
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-05-11 09:39:47 +0300

    add task1810

--- 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -5,6 +5,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*
+DownloadException
+*/
+
 public class Solution {
     public static void main(String[] args) throws DownloadException, IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
