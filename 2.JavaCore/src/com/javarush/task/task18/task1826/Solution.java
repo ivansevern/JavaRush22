92ab3c45eff94511dfb35e463424627c370f9046
Иван <ivansevern@gmail.com>
2023-03-30 12:48:26 +0300

    Solution 1826

--- 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
@@ -7,7 +7,21 @@ import java.io.IOException;
 Шифровка
 */
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String mode = args[0];
+        if (!(mode.equals("-e") || mode.equals("-d"))) return;
+        try (FileInputStream fileInputStream = new FileInputStream(args[1]);
+            FileOutputStream fileOutputStream = new FileOutputStream(args[2])) {
 
+            if (mode.equals("-e")) {
+                while (fileInputStream.available() > 0) {
+                    fileOutputStream.write(fileInputStream.read() + 32);
+                }
+            } else {
+                while (fileInputStream.available() > 0) {
+                    fileOutputStream.write(fileInputStream.read() - 32);
+                }
+            }
+        }
     }
 }
